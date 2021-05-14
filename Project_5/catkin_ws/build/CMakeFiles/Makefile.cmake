48eb838dbeacb51e9a21206ab769da7e13bb58d6
Felipe Nascimento Martins <felipe.n.martins@gmail.com>
2021-05-14 12:06:01 +0200

    fix: Upload new build and devel folders
    Upload new build and devel folders after creating and compiling the new add_markers_5s.cpp.

--- Project_5/catkin_ws/build/CMakeFiles/Makefile.cmake
+++ Project_5/catkin_ws/build/CMakeFiles/Makefile.cmake
@@ -371,6 +371,7 @@ set(CMAKE_DEPEND_INFO_FILES
   "gtest/gtest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
   "gtest/gtest/CMakeFiles/gtest.dir/DependInfo.cmake"
   "slam_gmapping/slam_gmapping/CMakeFiles/_catkin_empty_exported_target.dir/DependInfo.cmake"
+  "add_markers/CMakeFiles/add_markers_5s.dir/DependInfo.cmake"
   "add_markers/CMakeFiles/geometry_msgs_generate_messages_py.dir/DependInfo.cmake"
   "add_markers/CMakeFiles/std_msgs_generate_messages_py.dir/DependInfo.cmake"
   "add_markers/CMakeFiles/geometry_msgs_generate_messages_nodejs.dir/DependInfo.cmake"
---
+++ Project_5/catkin_ws/build/add_markers/CMakeFiles/add_markers_5s.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/add_markers_5s.dir/src/add_markers_5s.cpp.o"
+  "/home/workspace/catkin_ws/devel/lib/add_markers/add_markers_5s.pdb"
+  "/home/workspace/catkin_ws/devel/lib/add_markers/add_markers_5s"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/add_markers_5s.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
